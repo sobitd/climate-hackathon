b225fb9696c8f52c0623ace1932de90405c6c8b1
sobitd <sobitdaniel@gmail.com>
2022-10-20 14:48:18 +0300

    edit username model name to user

--- db/migrate/20221020114438_create_usernames.rb
+++ db/migrate/20221020114438_create_usernames.rb
@@ -1,6 +1,7 @@
 class CreateUsernames < ActiveRecord::Migration[7.0]
   def change
-    create_table :usernames do |t|
+    create_table :users do |t|
+      t.string :username
       t.string :password
 
       t.timestamps
